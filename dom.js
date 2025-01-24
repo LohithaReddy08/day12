2d3500730e2f7639bd0b58ad6a429f45daef4ccc
LohithaReddy08 <lohithasyamala2004@gmail.com>
2025-01-24 13:11:59 +0530

    training

---
+++ dom.js
@@ -0,0 +1,21 @@
+//let ele = document.getElementsByClassName("test");
+//console.log(ele);
+//console.log(Array.isArray(ele));
+//ele[0].innerText="Hello"
+//ele[0].style.backgroundColor="tomato"
+
+//! spread operator:- itwill takeout each and every value from the original array
+//! and store it inside one more new array(pure array).
+
+//let x=[...ele];
+//x.map(element=>{
+  //  element.style.backgroundColor="orange"
+//})
+
+//!
+// let ele = document.getElementByTagName("fromData");
+// [...ele].map(inp=>{
+//     console.log(ele);
+    let ele = document.querySelectorAll(".test");
+    console.log(ele);
+// })
