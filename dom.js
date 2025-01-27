f3a5a09a60a511768a7e715ef7e093a7f2002909
LohithaReddy08 <lohithasyamala2004@gmail.com>
2025-01-27 12:59:37 +0530

    training

--- dom.js
+++ dom.js
@@ -96,20 +96,65 @@
     // console.log(image);
     // document.body.appendChild(ele);
     // document.body.appendChild(image);
-    let form = document.querySelector("form");
-    let username = document.getElementById("uName");
-    let mail = document.getElementById("uMail");
-    let psw = document.getElementById("uPass");
+    // let form = document.querySelector("form");
+    // let username = document.getElementById("uName");
+    // let mail = document.getElementById("uMail");
+    // let psw = document.getElementById("uPass");
 
-    form.addEventListener("submit",(event)=>{
-        event.preventDefault();
-        let uname = username.value;
-        let uemail = mail.value;
-        let upass = psw.value;
-        let userDetails={
-            uname , uemail, upass
-        }
-        console.log(userDetails);
-        localStorage.setItem("userDetails" , JSON.stringify(userDetails))
+    // form.addEventListener("submit",(event)=>{
+    //     event.preventDefault();
+    //     let uname = username.value;
+    //     let uemail = mail.value;
+    //     let upass = psw.value;
+    //     let userDetails={
+    //         uname , uemail, upass
+    //     }
+    //     console.log(userDetails);
+    //     localStorage.setItem("userDetails" , JSON.stringify(userDetails))
 
-    })
+    // })
+
+    
+    // let h1 = document.createElement("h1");
+    // h1.setAttribute("id","demo");
+    // h1.innerText = ("MRU Batch3");
+    // console.log(h1);
+    // document.body.appendChild(h1);
+
+    let mainEle = document.createElement("div");
+    mainEle.setAttribute("id","mainBlock");
+    mainEle.style.border="2px solid red";
+    mainEle.style.width="550px";
+    mainEle.style.height="550px";
+
+    console.log(mainEle);
+
+    let topEle = document.createElement("div");
+    topEle.setAttribute("class","topBlock");
+
+    let image = document.createElement("img");
+    image.src="./photo-cat.jpeg";
+    image.style.width="550px"
+
+    let bottomEle = document.createElement("div");
+    bottomEle.setAttribute("class","bottomBlock");
+
+    let h1 = document.createElement("h1");
+    h1.innerText= "CAT";
+    h1.style.textAlign="center"
+
+    let btn = document.createElement("button");
+    btn.innerText="View More";
+    btn.style.border="none";
+    btn.style.padding="20px";
+    btn.style.backgroundcolor="dodgerblue";
+    btn.style.color="white";
+
+    bottomEle.appendChild(h1);
+    bottomEle.appendChild(btn);
+
+    topEle.appendChild(image);
+    mainEle.appendChild(topEle);
+    mainEle.appendChild(bottomEle);
+    document.body.appendChild(mainEle);
+    
