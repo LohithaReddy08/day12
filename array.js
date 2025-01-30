eba6e0c84e8c94cb2d39096feb49e3f6607b4c08
LohithaReddy08 <lohithasyamala2004@gmail.com>
2025-01-30 19:12:49 +0530

    training

---
+++ array.js
@@ -0,0 +1,14 @@
+let empdetails = {
+    empId : "1",
+    empName: "sravya",
+    empCompany: "google",
+    empSalary: "30,000",
+    empAddress: {
+        empCity : "hyd",
+        emparea : "hitechcity"
+    }
+}
+console.log(empdetails.empId);
+console.log(empdetails.empName);
+console.log(empdetails.empCompany);
+console.log(empdetails.empSalary);
